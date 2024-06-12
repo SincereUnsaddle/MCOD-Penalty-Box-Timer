e5420515b984eea90260f01ca77a05723ab1c80f
SincereUnsaddle <102601618+SincereUnsaddle@users.noreply.github.com>
2024-06-12 19:55:22 +0200

    fixed the folders

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'penalty-box-timer-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/stopwatch.js',
-  '/manifest.json',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
+  './',
+  './index.html',
+  './styles.css',
+  './stopwatch.js',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
