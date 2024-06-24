741091030cde353079701298a0a2f087f4242906
SincereUnsaddle <102601618+SincereUnsaddle@users.noreply.github.com>
2024-06-24 21:42:46 +0200

    june 2024 final v11
    added new settings menu
    added 2 new blockers as requested for edgecase when 2 blockers are standing and new ones come in
    added visual cue as requested, to give a short warning before "stand-up" is called.
    added function to change penalty duration and stopwatch colors.
    compacted the design, to hopefully make 10 stopwatches fit on small screens.

--- stopwatch.js
+++ stopwatch.js
@@ -2,33 +2,39 @@ document.addEventListener('DOMContentLoaded', function() {
     const stopwatches = [
         { id: 'whiteBlocker1', label: 'White Blocker 1' },
         { id: 'whiteBlocker2', label: 'White Blocker 2' },
+		{ id: 'whiteBlocker3', label: 'White Blocker 3' },
+		{ id: 'whiteBlocker4', label: 'White Blocker 4' },
         { id: 'whiteJammer', label: 'White Jammer' },
         { id: 'blackBlocker1', label: 'Black Blocker 1' },
         { id: 'blackBlocker2', label: 'Black Blocker 2' },
+		{ id: 'blackBlocker3', label: 'Black Blocker 3' },
+		{ id: 'blackBlocker4', label: 'Black Blocker 4' },
         { id: 'blackJammer', label: 'Black Jammer' }
     ];
 
     stopwatches.forEach(sw => {
+		const initialTime = 30 * timeMultiplier;
         const stopwatch = document.getElementById(sw.id);
 
         stopwatch.innerHTML = `
             <div class="message"></div>
             <div class="time-container">
-                <div class="time">30</div>
-                <button class="add-time-button" onclick="addTime('${sw.id}', 30)">+</button>
+                <div class="time">${initialTime}</div>
+               
             </div>
             <div class="controls">
                 <button class="start-button" onclick="toggleStopwatch('${sw.id}', this)">Start</button>
                 <button class="reset-button" onclick="resetStopwatch('${sw.id}')">Reset</button>
+				 <button class="add-time-button" onclick="addTime('${sw.id}', (30 * timeMultiplier))">+</button>
             </div>
         `;
     });
-
     updateMasterButton();
 });
 
 const intervals = {};
 const remainingTimes = {}; // Store remaining times in hundredths of a second
+let timeMultiplier = 1;
 
 function toggleStopwatch(id, button) {
     const stopwatch = document.getElementById(id);
@@ -38,11 +44,11 @@ function toggleStopwatch(id, button) {
     if (!intervals[id]) { // Timer is not running
         let startTime = Date.now();
 		if (id=='whiteJammer'&&remainingTimes['blackJammer']){ //update jammer time if jammer is in the box
-			remainingTimes['whiteJammer']=3000+(-remainingTimes['blackJammer']%3000);
+			remainingTimes['whiteJammer']=(3000 * timeMultiplier)+(-remainingTimes['blackJammer']%(3000 * timeMultiplier));
 			resetStopwatch('blackJammer');
 		}
 		if (id=='blackJammer'&&remainingTimes['whiteJammer']){
-			remainingTimes['blackJammer']=3000+(-remainingTimes['whiteJammer']%3000);
+			remainingTimes['blackJammer']=(3000 * timeMultiplier)+(-remainingTimes['whiteJammer']%(3000 * timeMultiplier));
 			resetStopwatch('whiteJammer');
 		}
 		
@@ -65,7 +71,7 @@ function toggleStopwatch(id, button) {
                 messageDiv.textContent = 'Go';
                 stopwatch.classList.remove('red');
                 stopwatch.classList.add('green');
-                timeDiv.textContent = '0:0';
+                timeDiv.textContent = '0"0';
                 clearInterval(intervals[id]);
                 delete intervals[id];
                 delete remainingTimes[id];
@@ -76,8 +82,14 @@ function toggleStopwatch(id, button) {
 
                 updateMasterButton(); // Ensure the master button state is updated
             } else {
-                if (remainingTimes[id] <= 1000) {
+                if (remainingTimes[id] <= 1100) {
+                    stopwatch.classList.add('yellow');
+                    messageDiv.textContent = '';
+                    timeDiv.textContent = (remainingTimes[id] / 100).toFixed(1).replace('.', '"');
+                } 
+				if (remainingTimes[id] <= 1000) {
                     stopwatch.classList.add('red');
+					stopwatch.classList.remove('yellow');
                     messageDiv.textContent = 'Stand Up';
                     timeDiv.textContent = (remainingTimes[id] / 100).toFixed(1).replace('.', '"');
                 } else {
@@ -111,6 +123,7 @@ function addTime(id, seconds) {
 	
 	messageDiv.textContent = '';
     stopwatch.classList.remove('red');
+	stopwatch.classList.remove('yellow');
 
     if (!intervals[id]) {
         // If the timer is not running, update the display immediately
@@ -135,9 +148,10 @@ function resetStopwatch(id) {
     const startButton = stopwatch.querySelector('.start-button');
 
     stopwatch.classList.remove('red');
+	stopwatch.classList.remove('yellow');
     stopwatch.classList.remove('green');
     messageDiv.textContent = '';
-    timeDiv.textContent = '30';
+    timeDiv.textContent = timeMultiplier * 30;
 
     // Change button to Start after reset
     startButton.textContent = 'Start';
@@ -186,43 +200,113 @@ function updateMasterButton() {
     const anyStopped = Object.keys(remainingTimes).length > 0;
 
     if (anyRunning) {
-        masterButton.textContent = 'Stop';
+        masterButton.textContent = 'Stop All';
         masterButton.classList.add('stop-button');
         masterButton.style.backgroundColor = '#ec008c';
+		masterButton.style.cursor = 'pointer';
         masterButton.disabled = false;
     } else if (anyStopped) {
         masterButton.textContent = 'Continue';
         masterButton.classList.remove('stop-button');
         masterButton.style.backgroundColor = 'black';
         masterButton.style.color = 'white';
+		masterButton.style.cursor = 'pointer';
         masterButton.disabled = false;
     } else {
         masterButton.textContent = 'Empty Box';
         masterButton.classList.remove('stop-button');
-        masterButton.style.backgroundColor = 'white';
+        masterButton.style.backgroundColor = '#ec008c';
         masterButton.style.color = 'black';
         masterButton.style.cursor = 'not-allowed';
         masterButton.disabled = true;
     }
 }
-document.addEventListener('DOMContentLoaded', function() {
-    const settingsIcon = document.getElementById('settings-icon');
-    const settingsMenu = document.getElementById('settings-menu');
-    const backgroundSelect = document.getElementById('background');
 
-    // Toggle visibility of settings menu
-    settingsIcon.addEventListener('click', function() {
-        if (settingsMenu.classList.contains('hidden')) {
-            settingsMenu.classList.remove('hidden');
-            settingsMenu.classList.add('visible');
-        } else {
-            settingsMenu.classList.remove('visible');
-            settingsMenu.classList.add('hidden');
+
+
+function saveSettings() {
+    const background = document.getElementById('background').value;
+    const team1Color = document.getElementById('team1-color').value;
+    const team2Color = document.getElementById('team2-color').value;
+	const team1Font = document.getElementById('team1fontColor').value;
+    const team2Font = document.getElementById('team2fontColor').value;
+    const penaltyDuration = parseInt(document.getElementById('penalty-duration').value); /*
+    const soundOption = document.getElementById('sound-option').checked; */
+	const settingsMenu = document.getElementById('settings-menu');
+	
+	
+	if (penaltyDuration !== (timeMultiplier * 30)) {
+        timeMultiplier = penaltyDuration / 30;
+
+        // Update the time displayed on each stopwatch
+        const stopwatches = document.querySelectorAll('.time');
+        stopwatches.forEach(timeDiv => {
+            const newTime = 30 * timeMultiplier;
+            timeDiv.textContent = newTime;
+        });
+
+        // Update remainingTimes to reflect the new timeMultiplier
+        for (let id in remainingTimes) {
+            if (remainingTimes.hasOwnProperty(id)) {
+                remainingTimes[id] = 3000 * timeMultiplier;
+            }
         }
-    });
+    }
+	document.body.style.background = background;
+	document.documentElement.style.setProperty('--team1Color', team1Color);
+    document.documentElement.style.setProperty('--team2Color', team2Color);
+	document.documentElement.style.setProperty('--team1fontColor', team1Font);
+    document.documentElement.style.setProperty('--team2fontColor', team2Font);
+	document.getElementById("settings-menu").style.width = "0%";
+	
+	
+	
+    /*setupStopwatches();
+    localStorage.setItem('penaltyBoxSettings', JSON.stringify({
+        background,
+        team1Color,
+        team2Color,
+        penaltyDuration,
+        soundOption
+    }));*/
+    
+	
+	 
+}
 
-    // Change background color
-    backgroundSelect.addEventListener('change', function() {
-        document.body.style.background = backgroundSelect.value;
+/* Open when someone clicks on the span element */
+function openNav() {
+	const background = document.getElementById('background');
+    const team1Color = document.getElementById('team1-color');
+    const team2Color = document.getElementById('team2-color');
+	const team1Font = document.getElementById('team1fontColor');
+    const team2Font = document.getElementById('team2fontColor');
+    const penaltyDuration = parseInt(document.getElementById('penalty-duration').value); /*
+    const soundOption = document.getElementById('sound-option').checked; */
+	const settingsMenu = document.getElementById('settings-menu');
+
+ settingsMenu.style.width = "100%";
+  background.addEventListener('change', function() {
+        document.getElementById("settings-tbl").style.background = background.value;
     });
-});
+	team1Color.addEventListener('change', function() {
+        document.getElementById("team1col").style.background = team1Color.value;
+		document.getElementById("team1fon").style.background = team1Color.value;
+    });
+	team2Color.addEventListener('change', function() {
+        document.getElementById("team2col").style.background = team2Color.value;
+		document.getElementById("team2fon").style.background = team2Color.value;
+    });
+	team1Font.addEventListener('change', function() {
+        document.getElementById("team1fon").style.color = team1Font.value;
+    });
+	team2Font.addEventListener('change', function() {
+        document.getElementById("team2fon").style.color = team2Font.value;
+    });
+	
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("settings-menu").style.width = "0%";
+}
